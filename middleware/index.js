d1b01bd27a017a521ad682b41320ace3e0ed4ce7
chakradhar123 <chakradharvasurama@gmail.com>
2021-06-09 06:58:48 +0530

    spelling

--- middleware/index.js
+++ middleware/index.js
@@ -1,5 +1,4 @@
-const Poll = require("../models/polls"),
-      Comment    = require("../models/comment")
+const Poll = require("../models/polls");
 //all the middleware goes here
 let middleWareObj = {};
 
@@ -25,28 +24,6 @@ else{
 }
 }
 
-middleWareObj.checkCommentOwnership = function(req,res,next){
-    if(req.isAuthenticated()){
-        Comment.findOne({_id:req.params.comment_id}).then(foundComment=>{
-            
-            
-                //does the user own campground
-                if(foundComment.author.id.equals(req.user._id) || req.user.isAdmin){
-                    next();
-                }
-                else{
-                    req.flash("error", "You dont have permission to do that");
-                    res.redirect("back");
-                }
-            
-    }).catch(err=>{req.flash("error", "Campgrounds not found!!")
-    res.redirect("back")});
-    }
-    else{
-        req.flash("error", "You need to be logged in to do that!!");
-        res.redirect("back");
-    }
-}
 
 middleWareObj.isLoggedIn = function(req,res,next){
     if(req.isAuthenticated()){
